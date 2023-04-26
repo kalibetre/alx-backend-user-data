b18dabafd783dd54564da5b4b7fd81d8c59eaac9
kalibetre <kalbetre@icloud.com>
2023-04-26 23:25:52 +0300

    formated log

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """
 Filtered Logger module
 """
+import logging
 import re
 from typing import List
 
@@ -15,3 +16,22 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         re.sub(r'=([^=]*)', f'={redaction}', v)
         if v.split('=')[0] in fields else v for v in message.split(separator)
     ])
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str] = None):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filters incoming records using filter_datum
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
