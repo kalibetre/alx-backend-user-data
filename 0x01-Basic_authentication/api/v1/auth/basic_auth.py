60e80501d38ba38920e1fd53846e66c4128913d2
kalibetre <kalbetre@icloud.com>
2023-05-02 19:07:17 +0300

    decoding b64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """
 BasicAuth module for the API
 """
+from base64 import b64decode
+
 from api.v1.auth.auth import Auth
 
 
@@ -18,3 +20,17 @@ class BasicAuth(Auth):
         ) is not str or authorization_header[:6] != 'Basic ':
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Returns the decoded value of a Base64 string
+        """
+        if base64_authorization_header is None or type(
+                base64_authorization_header) is not str:
+            return None
+        try:
+            b64_bytes = b64decode(base64_authorization_header)
+            return b64_bytes.decode('utf-8')
+        except Exception:
+            return None
