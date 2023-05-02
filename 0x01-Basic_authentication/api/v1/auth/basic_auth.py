04499f00d30f2731f1ce6d253db7bd8da780de4f
kalibetre <kalbetre@icloud.com>
2023-05-02 18:56:50 +0300

    fix pysty warning

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,10 +2,7 @@
 """
 BasicAuth module for the API
 """
-from typing import List, TypeVar
-
 from api.v1.auth.auth import Auth
-from flask import request
 
 
 class BasicAuth(Auth):
@@ -14,7 +11,7 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """
-        Returns the base64 part of the Authorization header 
+        Returns the base64 part of the Authorization header for a Basic Auth
         """
         if authorization_header is None or type(
                 authorization_header
