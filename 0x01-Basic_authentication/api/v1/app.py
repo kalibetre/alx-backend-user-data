be1e816c36fe7eb27762b097ac4bdaa11aaf61d5
kalibetre <kalbetre@icloud.com>
2023-05-02 17:26:59 +0300

    add unauthorized error handler

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,12 +2,12 @@
 """
 Route module for the API
 """
-from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
 import os
+from os import getenv
 
+from api.v1.views import app_views
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -19,6 +19,13 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort
 from api.v1.views import app_views
+from flask import abort, jsonify
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -12,6 +12,15 @@ def status() -> str:
       - the status of the API
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - abort(401)
+    """
+    abort(401)
 
 
 @app_views.route('/stats/', strict_slashes=False)
