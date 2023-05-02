41f03ed182ac13f471b04c6adbcbf4c0bbb410ec
kalibetre <kalbetre@icloud.com>
2023-05-02 19:43:31 +0300

    implement current_user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -68,3 +68,13 @@ class BasicAuth(Auth):
         except Exception:
             pass
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Returns the current user
+        """
+        header = self.authorization_header(request)
+        base64_header = self.extract_base64_authorization_header(header)
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        user_credentials = self.extract_user_credentials(decoded_header)
+        return self.user_object_from_credentials(user_credentials[0],
+                                                 user_credentials[1])
