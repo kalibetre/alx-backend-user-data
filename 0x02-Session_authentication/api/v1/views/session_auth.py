7a392e67705b93e0475cb974ce432d51d5c237fe
kalibetre <kalbetre@icloud.com>
2023-05-04 00:01:08 +0300

    impl session login

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" Module of Users views
+"""
+from os import getenv
+
+from api.v1.views import app_views
+from flask import jsonify, request
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ POST /auth_session/login
+    Return:
+      - User object JSON represented
+      - Sets a cookie for the user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or email == '':
+        return jsonify({"error": "email missing"}), 400
+    if password is None or password == '':
+        return jsonify({"error": "password missing"}), 400
+    print(email, password)
+    user = User.search({'email': email})
+    if len(user) == 0:
+        return jsonify({"error": "no user found for this email"}), 404
+    user = user[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    cookie_name = getenv('SESSION_NAME')
+    response = jsonify(user.to_json())
+    response.set_cookie(cookie_name, session_id)
+    return response
