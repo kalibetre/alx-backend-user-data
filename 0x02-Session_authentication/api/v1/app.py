011a12eda066d42fb6c0a263ae033d8a965322b5
kalibetre <kalbetre@icloud.com>
2023-05-03 22:46:45 +0300

    auth session endpoint protection

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -58,9 +58,11 @@ def before_request() -> str:
                 '/api/v1/status/',
                 '/api/v1/unauthorized/',
                 '/api/v1/forbidden/',
+                '/api/v1/auth_session/login/',
             ],
     ):
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None and auth.session_cookie(
+                request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
