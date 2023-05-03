3c4fac263dbac7d19c0ca5caa24e7f4a6a6d4c39
kalibetre <kalbetre@icloud.com>
2023-05-03 22:26:15 +0300

    add empty session auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+BasicAuth module for the API
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """A session auth class to manage the API authentication"""
+    pass
