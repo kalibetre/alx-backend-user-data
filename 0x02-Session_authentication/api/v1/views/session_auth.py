d3a5a71feab93d8b852739d312aa61ecab501ca2
kalibetre <kalbetre@icloud.com>
2023-05-04 00:15:10 +0300

    impl session logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -4,7 +4,7 @@
 from os import getenv
 
 from api.v1.views import app_views
-from flask import jsonify, request
+from flask import abort, jsonify, request
 from models.user import User
 
 
@@ -34,3 +34,18 @@ def login() -> str:
     response = jsonify(user.to_json())
     response.set_cookie(cookie_name, session_id)
     return response
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def logout() -> str:
+    """ DELETE /auth_session/logout
+    Return:
+      - Empty JSON
+      - Deletes the user session / logout
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request) is False:
+        abort(404)
+    return jsonify({}), 200
