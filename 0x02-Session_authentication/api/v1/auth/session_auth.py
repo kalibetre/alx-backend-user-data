a80af57c1f6675f6ef26dd0a5e0a7e8e46a9c10e
kalibetre <kalbetre@icloud.com>
2023-05-03 22:30:37 +0300

    impl create_session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,19 @@
 """
 BasicAuth module for the API
 """
+from uuid import uuid4
+
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """A session auth class to manage the API authentication"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id"""
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
