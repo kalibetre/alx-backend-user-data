c3042c641155705231056355a3c871cedd2484b4
kalibetre <kalbetre@icloud.com>
2023-05-09 12:32:53 +0300

    impl pwd hashing

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Auth Modules
+"""
+import bcrypt
+
+
+def _hash_password(password: str):
+    """Hashes a plain string password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
