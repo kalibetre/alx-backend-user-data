570ff61b73bc73b91d9afd6e9f70a9fdd03d3145
kalibetre <kalbetre@icloud.com>
2023-05-10 11:14:00 +0300

    impl create_session on auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -53,3 +53,16 @@ class Auth:
                                   user.hashed_password)
         except Exception:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Creates a session ID for the user email
+        Returns:
+            - the session ID as a string
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except Exception:
+            return None
