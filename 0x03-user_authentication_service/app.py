3a9ab9b546f0c571e83242a3a04bf5c8e2c7ecc4
kalibetre <kalbetre@icloud.com>
2023-05-10 10:56:12 +0300

    add users endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,11 @@
 """
 Flask App
 """
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -16,5 +18,20 @@ def getMessage() -> str:
     return jsonify(message="Bienvenue")
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user() -> str:
+    """ POST /users
+    Return:
+        - registered user emails
+    """
+    try:
+        email = request.form['email']
+        password = request.form['password']
+        user = AUTH.register_user(email=email, password=password)
+        return jsonify(email=user.email, message='user created'), 201
+    except ValueError:
+        return jsonify(message='email already registered'), 400
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5001')
