677453655a0b261bae4818da27aa5beeddbabe57
kalibetre <kalbetre@icloud.com>
2023-05-10 11:44:00 +0300

    add get pwd reset token endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -82,5 +82,19 @@ def profile() -> str:
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """ POST /reset_password
+    Return:
+        - reset password token
+    """
+    try:
+        email = request.form['email']
+        token = AUTH.get_reset_password_token(email=email)
+        return jsonify(email=email, reset_token=token), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
