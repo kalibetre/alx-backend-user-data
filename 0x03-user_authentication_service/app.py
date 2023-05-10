ef68a831568142a79390dd75bdecc3f08664063a
kalibetre <kalbetre@icloud.com>
2023-05-10 11:51:09 +0300

    add pwd update endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -96,5 +96,21 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
+@app.route('/update_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """ PUT /update_password
+    Return:
+        - update password
+    """
+    try:
+        email = request.form['email']
+        token = request.form['reset_token']
+        new_password = request.form['new_password']
+        AUTH.update_password(reset_token=token, password=new_password)
+        return jsonify(email=email, message='Password updated'), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
